33dd431467d1fb720044a16bf4b951b53c75e0a0
ved0099-vsd <veddhamal0099@gmail.com>
2026-09-07 23:48:26 +0530

    Pattern
    For Loop Star patterns etc

---
+++ ForLoopNoPattern.py
@@ -0,0 +1,11 @@
+def main():
+    n = int(input("Enter a number : "))
+
+    for i in range(1,n+1):
+        for j in range(1,i +1):
+
+            print(j, end = "")
+        print()
+
+if __name__ == "__main__":
+    main()
---
+++ ForLoopStarPattern.py
@@ -0,0 +1,9 @@
+def main():
+    n = int(input("Enter number of rows: "))
+
+    for i in range(1, n + 1):
+        print("*" * i)
+
+
+if __name__ == "__main__":
+    main()
