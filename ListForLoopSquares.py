1b89d612dbbda2b6f7bed23bb003d71e03be3c49
ved0099-vsd <veddhamal0099@gmail.com>
2026-09-12 20:49:53 +0530

    List For Loop

---
+++ ListForLoopSquares.py
@@ -0,0 +1,11 @@
+# Square of every element
+
+def main():
+    Data = [2,4,6,8]
+
+    for i in Data:
+        Ans = i * i
+        print("Square of",i,"is:",Ans)
+
+if __name__ == "__main__":
+    main()
---
+++ ListForLoopprintGreater25.py
@@ -0,0 +1,9 @@
+def main():
+    Data = [10,30,15,40,20,50]
+
+    for i in Data:
+        if i > 25:
+            print(i)
+
+if __name__ == "__main__":
+    main()
