1d214851ade46e03d94ff866107bc3f9036371ed
ved0099-vsd <veddhamal0099@gmail.com>
2026-09-23 15:55:14 +0530

    ProcessID

---
+++ ProcessID.py
@@ -0,0 +1,5 @@
+import os
+
+print("PID of current process is :",os.getpid()) # Current Process
+
+print("PID of Parent process is : ",os.getppid()) #Parent Process
---
+++ ProcessID1.py
@@ -0,0 +1,4 @@
+import os
+
+print("PID of current process  :",os.getpid())
+print("PID of Parnet process is :",os.getppid())
