23e5560129d37d968f975761531f258e5cf03b22
ved0099-vsd <veddhamal0099@gmail.com>
2026-09-24 13:45:48 +0530

    Exception Handling

---
+++ ExceptionDemo1.py
@@ -0,0 +1,10 @@
+def main():
+    No1 = int(input("Enter first number : "))
+    No2 = int(input("Enter second number : "))
+
+    Ans = No1 / No2
+
+    print("Ans is :",Ans)     #Zero division error
+
+if __name__ == "__main__":
+    main()
